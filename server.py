b141929feadce865ac8fab13852e2f042e830826
Logan Rickert <logan@rickertcorp.com>
2015-03-30 17:50:35 -0400

    Can now send multiple messages.

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python 
 
+# Python 2
+
 """ 
 A simple echo server 
 """ 
@@ -8,14 +10,15 @@ import socket
 
 host = '' 
 port = 8080 
-backlog = 5 
+backlog = 5
 size = 1024 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.bind((host,port)) 
 s.listen(backlog) 
 while 1: 
     client, address = s.accept() 
-    data = client.recv(size) 
-    if data: 
-        client.send(data) 
+    data = client.recv(size)
+    if data:
+    	print(data)
+        client.send(data)
     client.close()
