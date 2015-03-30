24ebe2190a8671a6ba6abdebf481fb1e5d4b22e8
Logan Rickert <logan@rickertcorp.com>
2015-03-30 17:40:39 -0400

    Now takes in username and echos message

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python 
 
+# Python 2
+
 """ 
 A simple echo client 
 """ 
@@ -9,9 +11,10 @@ import socket
 host = 'localhost' 
 port = 8080 
 size = 1024 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host,port)) 
-s.send('Hello, world') 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+name = raw_input("What do you want your name to be? ")
+s.connect((host,port))
+s.send(name + ": " + raw_input("What would you like to send? "))
 data = s.recv(size) 
 s.close() 
-print 'Received:', data
+print(data)
